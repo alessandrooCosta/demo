e5383e859a2453c80daefd609a6a236070cb4ec6
Alessandro <alessandrocosta103@gmail.com>
2024-07-08 16:19:12 -0300

    ajustes na gitignore

---
+++ src/main/java/com/example/demo/ScreenMatchApplication.java
@@ -0,0 +1,21 @@
+package com.example.demo;
+
+import com.example.demo.service.ConsumoAPI;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScreenMatchApplication implements CommandLineRunner {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScreenMatchApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		var consumo = new ConsumoAPI();
+		var json = consumo.obterDados("http://www.omdbapi.com/?t=gilmore+girls&Season=1&apikey=a74872f8");
+		System.out.println(json);
+	}
+}
